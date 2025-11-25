d78c89ab9b188d8da060a1c2c005332d174cd869
Anastasia <pomelova-2004@mail.ru>
2025-11-25 22:32:48 +0700

    убрала лишние файлы

--- 7-8/build/mp3_to_pcm.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import librosa
-from pydub import AudioSegment
-import os
-
-y, sr = librosa.load("audio.mp3", sr=44100, mono=True)
-pcm_data = (y * 32767).astype(np.int16)
-pcm_data.tofile("audio.pcm")
